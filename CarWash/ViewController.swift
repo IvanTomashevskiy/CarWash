cf77ea3864c2f7d543f7d25596a693ec06a0a28c
Ivan Tomashevskiy <tomashevskiy_vanya@mail.ru>
2020-07-14 19:19:25 +0300

    start

---
+++ CarWash/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CarWash
+//
+//  Created by Иван on 14.07.2020.
+//  Copyright © 2020 Ivan Tomashevskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
